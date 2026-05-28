79a98baa3c0705a450491f25c1a385fc0d3d46ba
abdullahhelmy114 <abdullahhelmy114@gmail.com>
2026-05-28 21:17:33 +0300

    sign

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -1,7 +1,9 @@
 import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
@@ -10,7 +12,7 @@ const transporter = nodemailer.createTransport({
 
 export async function sendEmail(to: string, subject: string, html: string) {
   await transporter.sendMail({
-    from: `Ruhulqudus Academy <${process.env.EMAIL_FROM}>`,
+    from: `Ruhulqudus Academy <${process.env.EMAIL_USER}>`,
     to,
     subject,
     html,
