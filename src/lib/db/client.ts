f7d9a65e650b45fc4ae0b3c99b88eacdb3bea34f
abdullahhelmy114 <abdullahhelmy114@gmail.com>
2026-06-24 06:01:37 +0300

    new sistem for courses

--- src/lib/db/client.ts
+++ src/lib/db/client.ts
@@ -1,6 +1,6 @@
 import { neon, neonConfig } from '@neondatabase/serverless';
 
-neonConfig.fetchConnectionCache = true;
+// neonConfig.fetchConnectionCache = true;  <-- تم إيقاف هذا السطر لأنه أصبح قديماً ويعمل تلقائياً
 
 const sql = neon(process.env.DATABASE_URL!);
 
