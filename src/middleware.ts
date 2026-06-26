d6caf055cb34c214e0245fe072150bfb0d7ebf51
abdullahhelmy114 <abdullahhelmy114@gmail.com>
2026-06-27 00:22:47 +0300

    new sistem for courses

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,17 +2,10 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const sessionCookie = request.cookies.get("__session")?.value;
-
-  const isProtected =
-    request.nextUrl.pathname.startsWith("/dashboard") ||
-    request.nextUrl.pathname.startsWith("/profile") ||
-    request.nextUrl.pathname.startsWith("/live");
-
-  if (isProtected && !sessionCookie) {
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
-
+  // قمنا بإيقاف الطرد الأعمى من هنا.
+  // السيرفر سيسمح بالمرور، وسنعتمد كلياً على حماية (AdminLayout / TeacherLayout)
+  // لأنها أدق وتفحص حالة فايربيز الحقيقية بدلاً من الاعتماد على الـ Cookies التي قد تتأخر.
+  
   return NextResponse.next();
 }
 
